0df62f38b09bfb7963ac36252814241b7579cc6f
Unagi-zoso <Unagi.zoso@gmail.com>
2025-03-12 15:13:53 +0900

    test

---
+++ src/main/java/org/bob/siungongsi/SiungongsiApplication.java
@@ -0,0 +1,12 @@
+package org.bob.siungongsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiungongsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiungongsiApplication.class, args);
+	}
+}
